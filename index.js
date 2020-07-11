d3b4c6732fa0f0a95a0514a82ab148147e89eb9b
Serkane <serkane@jaina.home>
2020-07-11 23:29:50 +0200

    nav

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-(function () {
-	var essay = document.getElementsByClassName("essay")[0];
-	var title = document.getElementsByClassName("title");
 
-	essay.style.backgroundColor = "lightblue";
-})();
+var isNavOpen = false;
+
+var nav = document.getElementsByTagName("nav")[0];
+
+function openNav(){
+	nav.style.width = "20vw";
+	nav.style.right = -Math.sin(10*2*Math.PI/360)*100+"vh";
+	nav.style.transform = "rotate(10deg)";
+	isNavOpen = true;
+}
+function closeNav(){
+	nav.style.width = "0";
+	nav.style.right = "0";
+	nav.style.transform = "rotate(0deg)";
+	isNavOpen = false;
+}
+
+function toggleNav(){
+	isNavOpen?closeNav():openNav();
+}
+
+nav.addEventListener("mouseenter", openNav);
+nav.addEventListener("mouseleave", closeNav);
+
