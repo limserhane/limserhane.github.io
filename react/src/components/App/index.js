5c7e7ffe55d004cebe7686ccde1c287238113e34
limserhane <imserkane@gmail.com>
2020-07-18 13:12:46 +0200

    structure

--- react/src/components/App/index.js
+++ react/src/components/App/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./styles.css";
+
+import Header from "../Header";
 
 function App() {
 	return (
 		<div className="app-container">
-			<header>
-				<h1>/home/limserhane</h1>
-				<a href="#">≡</a>
-			</header>
+			<Header />
 		</div>
 	);
 }
